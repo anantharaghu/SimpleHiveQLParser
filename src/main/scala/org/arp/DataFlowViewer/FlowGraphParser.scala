0ded784d7867fa2e485a1d08ca59b9311b18f707
anana1 <anantharaghu.ananthanarayanan@wal-mart.com>
2018-02-05 17:01:31 -0600

    Parse multiple files under a folder

--- src/main/scala/org/arp/DataFlowViewer/FlowGraphParser.scala
+++ src/main/scala/org/arp/DataFlowViewer/FlowGraphParser.scala
@@ -1,41 +1,47 @@
 package org.arp.DataFlowViewer
 
+import java.io.File
+
 object FlowGraphParser {
+  val sourceTablePattern = "FROM|JOIN"
+  val insertTablePattern = "INSERT(\\s+OVERWRITE|\\s+INTO)\\s+TABLE"
+  val createTablePattern = "CREATE\\s+TABLE(\\s+IF\\s+NOT\\s+EXISTS)?"
+  val patterns = s"(?i)($createTablePattern|$insertTablePattern|$sourceTablePattern)\\s+\\S+\\w+".r
+  val graph = new FlowGraph
 
-    def main (args : Array[String]) {
-      //val lines = "create table xyz from schema.table inner join  sch1.table1  where; insert into table xyz (select * from  sch1.table1);".toUpperCase()
-      val queryFile = "/Users/Raghu/test.sql"
-      val lines = scala.io.Source.fromFile(queryFile).mkString(" ")
-      val sourceTablePattern = "FROM|JOIN"
-      val insertTablePattern = "INSERT(\\s+OVERWRITE|\\s+INTO)\\s+TABLE"
-      val createTablePattern = "CREATE\\s+TABLE(\\s+IF\\s+NOT\\s+EXISTS)?"
-      val patterns = s"(?i)($createTablePattern|$insertTablePattern|$sourceTablePattern)\\s+\\S+\\w+".r
-      println(patterns)
-      val graph = new FlowGraph
+  def main(args: Array[String]) {
+    //val lines = "create table xyz from schema.table inner join  sch1.table1  where; insert into table xyz (select * from  sch1.table1);".toUpperCase()
+    val queryFolder = "\\\\dwshome-a.homeoffice.wal-mart.com\\dwsuserdata$\\anana1\\Documents\\assortment_analytics\\ao_base_data_layer\\oozie\\load_base_data_layer"
+    val files = new java.io.File(queryFolder).listFiles
+    files.foreach(parseQueries(_))
+    println(patterns)
+  }
 
-      for (query <- lines.split(";")) {
-        var node : Node = null
-        for (str <- patterns.findAllIn(query)){
-          if (str.matches(s"\\A($createTablePattern)\\s+.*")) {
-            val tableName = str.substring(str.lastIndexOf(" "))
-            node = new Node(tableName.trim)
-            graph.addNode(new Node(tableName.trim))
-            println("Added Node : " + tableName.trim)
-          }
-          if (str.matches(s"\\A($insertTablePattern)\\s+.*")) {
-            val tableName = str.substring(str.lastIndexOf(" "))
-            node = new Node(tableName.trim)
-          }
-          if (str.matches(s"\\A($sourceTablePattern)\\s+.*")) {
-            val tableName = str.substring(str.lastIndexOf(" "))
-            graph.addSource(node, tableName.trim)
-          }
-          println(s"$str ")
+  def parseQueries(fileName: File) {
+    val lines = scala.io.Source.fromFile(fileName).mkString(" ")
+    for (query <- lines.split(";")) {
+      var node: Node = null
+      for (str <- patterns.findAllIn(query)) {
+        if (str.matches(s"\\A($createTablePattern)\\s+.*")) {
+          val tableName = str.substring(str.lastIndexOf(" "))
+          node = new Node(tableName.trim)
+          graph.addNode(new Node(tableName.trim))
+          println("Added Node : " + tableName.trim)
         }
-        println("------")
+        if (str.matches(s"\\A($insertTablePattern)\\s+.*")) {
+          val tableName = str.substring(str.lastIndexOf(" "))
+          node = new Node(tableName.trim)
+        }
+        if (str.matches(s"\\A($sourceTablePattern)\\s+.*")) {
+          val tableName = str.substring(str.lastIndexOf(" "))
+          graph.addSource(node, tableName.trim)
+        }
+        println(s"$str ")
       }
-
-      println(graph.toString)
+      println("------")
     }
 
+    println(graph.toString)
   }
+
+}
